7993fe6a2caaf653f8e5400ccfbd496bc5db12f3
panyenling <miraclepan@hotmail.com>
2019-12-31 12:06:15 +0800

    add word count correct version

--- read.py
+++ read.py
@@ -6,9 +6,28 @@ with open('reviews.txt', 'r') as f:
         count += 1
         if count % 100000 == 0:
         	print(len(data))
-
 print('檔案讀取完了,總共有', len(data), '筆資料')
 print(data[0])
+
+sum_len = 0
+for d in data:
+	sum_len += len(d)
+print('留言的總長度', sum_len)
+print('留言的平均長度為', sum_len/len(data))
+new = []
+for d in data:
+	if len(d) < 100:
+		new.append(d)
+print('一共有', len(new), '筆留言長度小於100')
+print(new[0])
+print(new[1])
+
+good = []
+for d in data:
+	if 'good' in d:
+		good.append(d)
+print('一共有', len(good), '筆留言提到good')
+print(good[0])
 
 wc = {} #word_count
 for d in data:
@@ -17,8 +36,7 @@ for d in data:
 		if word in wc:
 			wc[word] += 1
 		else:
-			wc[word] = 1 #新增新的key進字典
-
+			wc[word] = 1
 for word in wc:
 	if wc[word] > 1000000:
 		print(word, wc[word])
@@ -29,10 +47,8 @@ while True:
 	word = input('請問你想查什麼字: ')
 	if word == 'q':
 		break
-	if word in wc:
-		print(word, '出現過的次數為: ', wc[word])
-	else:
-		print('這個字没有出現過哦!')
+	print(word,'出現過的次數為: ', wc[word])
+
 print('感謝使用本查詢功能')
 
 
@@ -53,46 +69,6 @@ print('感謝使用本查詢功能')
 
 
 
-
-
-
-
-
-
-
-
-
-
-# sum_len = 0
-#for d in data:
-# 	sum_len += len(d)
-# print('留言的總長度', sum_len)
-# print('留言的平均長度為', sum_len/len(data))
-
-# new = []
-# for d in data:
-# 	if len(d) < 100:
-# 		new.append(d)
-# print('一共有', len(new), '筆留言長度小於100')
-# print(new[0])
-# print(new[1])
-
-#new = [d for d in data if len(d) < 100]
-
-# good = []
-# for d in data:
-# 	if 'good' in d:
-# 		good.append(d)
-# print('一共有', len(good), '筆留言提到good')
-# print(good[0])
-
-#good = [d for d in data if 'good' in d]
-
-# count = 0
-# for review in good[:6]:
-# 	count += 1
-# 	print('這是第', count, '筆留言提到good')
-# 	print(review)
 
 
 
